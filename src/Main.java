ba58b8a878efdc564bf8ffb4789ca468e75ba8e7
Sumit Haswar <sumithaswar@gmail.com>
2025-10-13 11:03:59 -0700

    myHeap implementation

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import heap.Heap;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
@@ -5,9 +7,24 @@ void main() {
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    arrayList.add(4);
+    arrayList.add(9);
+    arrayList.add(12);
+    arrayList.add(3);
+    arrayList.add(2);
+    arrayList.add(1);
+    arrayList.add(48);
+    arrayList.add(94);
+
+    Heap<Integer> myHeap = new Heap<>(arrayList);
+    printList(myHeap.getHeapItems());
+    myHeap.push(900);
+
+    IO.println(myHeap.pop());
+}
+
+public static void printList(List<Integer> list){
+    String result = list.stream().map(String::valueOf).collect(Collectors.joining(", "));
+    IO.println(result);
 }
--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -1,38 +1,105 @@
 package heap;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Heap<T extends Comparable<T>> {
+    private final List<T> heapItems = new ArrayList<>();
+    private int heapSize = 0;
 
-    public Heap(List<T> items){
-
+    public Heap(List<T> items) {
+        for (T item : items) {
+            this.heapItems.add(item);
+        }
+        this.heapSize = items.size();
+        this.buildMaxHeap();
     }
 
-    public T peek(){
-
+    public List<T> getHeapItems() {
+        return this.heapItems;
     }
 
-    public void push(T element){
-
+    public T peek() {
+        if (heapSize <= 0) {
+            return null;
+        }
+        return this.heapItems.getFirst();
     }
 
-    public T pop(){
-        
+    public void push(T element) {
+        this.heapItems.add(element);
+        this.heapSize++;
+        this.incKey(element, this.heapSize - 1);
     }
 
-    public void heapify(){
+    public T pop() {
+        T top = this.heapItems.getFirst();
+        // move first element to last element
+        this.swap(0, this.heapSize - 1);
+        this.heapItems.removeLast();
+        this.heapSize--;
 
+        this.heapify(0, this.heapSize - 1);
+        return top;
     }
 
-    private int getLeft(int idx){
+    private void buildMaxHeap() {
+        int mid = getMid(0, this.heapSize - 1);
+        for (int i = mid; i >= 0; i--) {
+            this.heapify(i, this.heapSize - 1);
+        }
+    }
+
+    private void incKey(T newVal, int idx) {
+        while (idx >= 0 && this.heapItems.get(idx).compareTo(this.heapItems.get(this.getParent(idx))) > 0) {
+            this.swap(idx, this.getParent(idx));
+            idx = this.getParent(idx);
+        }
+    }
+
+    private void heapify(int idx, int lastIdx) {
+        // start from mid and keep heapifying till 0
+        int leftIdx = getLeft(idx);
+        int rightIdx = getRight(idx);
+
+        int largestIdx = idx;
+
+        // compare with left
+        if (leftIdx <= lastIdx && this.heapItems.get(leftIdx).compareTo(this.heapItems.get(idx)) > 0) {
+            largestIdx = leftIdx;
+        }
+
+        // compare with right
+        if (rightIdx <= lastIdx && this.heapItems.get(rightIdx).compareTo(this.heapItems.get(largestIdx)) > 0) {
+            largestIdx = rightIdx;
+        }
+
+        if (largestIdx != idx) {
+            // swap and recur heapify
+            this.swap(largestIdx, idx);
+            heapify(largestIdx, lastIdx);
+        }
+    }
+
+    private void swap(int idxA, int idxB) {
+        T temp = this.heapItems.get(idxA); // A
+        this.heapItems.set(idxA, this.heapItems.get(idxB)); // B <-- A
+        this.heapItems.set(idxB, temp); // B <-- temp
+    }
+
+    private int getMid(int left, int right) {
+        return left + ((right - left) / 2);
+    }
+
+    private int getLeft(int idx) {
         return 2 * idx + 1;
     }
 
-    private int getRight(int idx){
+    private int getRight(int idx) {
         return 2 * idx + 2;
     }
 
-    private int getParent(int idx){
+    private int getParent(int idx) {
         return (idx - 1) / 2;
     }
 
